dcefdfaf44fa4fce7489432c80d599113f031d81
Tom Ziegler <37410693+tfziegler@users.noreply.github.com>
2022-09-18 10:46:52 -0400

    Add files via upload

---
+++ Creates_a_new_text_file.ps1
@@ -0,0 +1,13 @@
+﻿###################################################################
+#
+# Title: Creates_a_new_text_file.ps1
+# Author: Tom Ziegler
+# Company: Microsoft
+# Last Modified: 9-15-2022
+# Version: 1.0
+#
+# Description - Creates a new text file
+###################################################
+
+# Creates a new text file
+New-Item -Path '\\servername\sharename\HeartbeatFailures.txt'
---
+++ Get_Heartbeat_Failure_alerts_greater_than-x-days.ps1
@@ -0,0 +1,20 @@
+﻿###################################################################
+#
+# Title: Get Heartbeat Failure alerts greater than "x" days
+# Author: Tom Ziegler
+# Company: Microsoft
+# Last Modified: 9-15-2022
+# Version: 1.0
+#
+# Description - Gets Heartbeat Failure Alerts
+###################################################################
+
+# Import Operations Manager Module and Connect to Management Server 
+Import-Module OperationsManager
+New-SCOMManagementGroupConnection YOUR-MANAGEMENTSERVER-NAME
+
+# Set the number of Days that will be used for Alert
+$AgeDays = 30
+
+# Returns SCOM alert based off number of days specified and writes to a file
+Get-SCOMAlert | where {$_.Name -eq 'Health Service Heartbeat Failure' -and $_.ResolutionState -ne '255' -and $_.TimeRaised -le (Get-Date).AddDays(-$AgeDays)} | Sort TimeRaised -Descending | select -expand MonitoringObjectDisplayName | Out-File -FilePath '\\servername\sharename\HeartbeatFailures.txt'
